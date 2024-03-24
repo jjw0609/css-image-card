af7c78a10a65cdb3c4ee0c928ffed5b374a5c8cc
Jung JaeWoong <jjw_0609@naver.com>
2024-03-24 21:41:50 +0900

    -

--- js/app.js
+++ js/app.js
@@ -1,35 +1,30 @@
 import apiKey from "./apiKey.js";
 
 (function () {
-    fetch(`https://pokeapi.co/api/v2/pokemon/5`)
+    fetch(`https://pokeapi.co/api/v2/pokemon/4`)
         .then(res => res.json())
         .then(res => {
-            const html = `
-                <div class="overlay"></div>
-                <img class="card" src="${res.sprites.front_default}" width="200" height="300" />
-            `;
 
-            document.querySelector('#content').innerHTML = html;
+            document.querySelector('#container .card').style.backgroundImage = `url(${res.sprites.front_default})`;
 
-
-
-            var container = document.querySelector('.container')
-            var overlay = document.querySelector('.overlay')
-            container.addEventListener('mousemove', function(e){
-                var x = e.offsetX
-                var y = e.offsetY
-                var rotateY = -1/5 * x + 20
-                var rotateX = 4/30 * y - 20
-
-                overlay.style = `background-position : ${x/5 + y/5}%; filter : opacity(${x/200}) brightness(1.2)`
-
-                container.style = `transform : perspective(350px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`
-            })
-
-            container.addEventListener('mouseout', function(){
-                overlay.style = 'filter : opacity(0)'
-                container.style = 'transform : perspective(350px) rotateY(0deg) rotateX(0deg)'
-            })
         })
 
+
+    var container = document.querySelector('#container');
+    var overlay = document.querySelector('.overlay');
+    container.addEventListener('mousemove', function(e){
+        var x = e.offsetX
+        var y = e.offsetY
+        var rotateY = -1/5 * x + 20
+        var rotateX = 4/30 * y - 20
+
+        overlay.style = `background-position : ${x/5 + y/5}%; filter : opacity(${x/100}) brightness(1.2)`
+
+        container.style = `transform : perspective(350px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`
+    });
+
+    container.addEventListener('mouseout', function(){
+        overlay.style = 'filter : opacity(0)'
+        container.style = 'transform : perspective(350px) rotateY(0deg) rotateX(0deg)'
+    });
 })();
